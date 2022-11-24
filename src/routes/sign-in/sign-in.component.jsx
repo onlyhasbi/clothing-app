93fefb88d24f4bcf0551c9b92f0e6aed2bf5828c
Muhammad Hasbi <onlyhasbi@gmail.com>
2022-11-24 16:29:44 +0800

    sign in functional using pop up and redirect method

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,39 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user);
+  const getResult = async () => {
+    const response = await getRedirectResult(auth);
+    if (response) {
+      await createUserDocumentFromAuth(response.user);
+    }
   };
+
+  useEffect(() => {
+    getResult();
+  }, []);
+
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    if (response) {
+      await createUserDocumentFromAuth(response.user);
+    }
+  };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In with Google</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign In with Google Redirect
+      </button>
     </div>
   );
 };
